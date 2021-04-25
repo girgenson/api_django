9d212b7fcf83037e9af100e96da2528fcfe607e8
grgnson <example@example.com>
2021-04-26 02:58:19 +0300

    Things are ready for API building

--- src/post_in/accounts/models.py
+++ src/post_in/accounts/models.py
@@ -1,43 +1,44 @@
 from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
-from django.db.models import (EmailField, CharField, BooleanField, DateTimeField, )
+from django.contrib.auth.hashers import make_password, identify_hasher
+from django.db.models import (EmailField, CharField, BooleanField, DateTimeField)
 
 
 class UserManager(BaseUserManager):
     use_in_migrations = True
 
-    def create_user(self, email, password=None, first_name=None, last_name=None,
-                     is_active=None, is_staff=None, is_admin=None, **extra_fields):
+    def create_user(self, email, password=None, name=None, full_name=None,
+                    is_active=True, is_staff=None, is_admin=None):
         """
         Create and save a user with the given username, email, and password.
         """
         if not email:
-            raise ValueError('Нужно ввести имейл')
+            raise ValueError('Пользователь должен иметь и-мэйл адрес')
         if not password:
-            raise ValueError('Нужно ввести пароль')
+            raise ValueError('Пользователь должен ввести пароль')
         email = self.normalize_email(email)
-        user = self.model(email=email, first_name=first_name, **extra_fields)
+        user = self.model(email=email, name=name)
         user.set_password(password)
-        user.is_staff = is_staff
+        user.staff = is_staff
         user.admin = is_admin
         user.is_active = is_active
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password=None, first_name=None, **extra_fields):
-        user = self.create_user(email, first_name=first_name, password=password,
+    def create_superuser(self, email, password=None, name=None):
+        user = self.create_user(email, name=name, password=password,
                                 is_staff=True, is_admin=True)
         return user
 
-    def create_staff_user(self, email, password=None, first_name=None, **extra_fields):
-        user = self.create_user(email, first_name=first_name, password=password,
-                                is_staff=True, is_admin=True)
+    def create_staffuser(self, email, password=None, name=None):
+        user = self.create_user(email, name=name, password=password,
+                                is_staff=True, is_admin=False)
         return user
 
 
 class User(AbstractBaseUser):
     email = EmailField(unique=True, max_length=255)
-    first_name = CharField(max_length=255, blank=True, null=True)
-    last_name = CharField(max_length=255, blank=True, null=True)
+    name = CharField(max_length=255, blank=True, null=True)
+    full_name = CharField(max_length=255, blank=True, null=True)
     staff = BooleanField(default=False)
     is_active = BooleanField(default=True)
     admin = BooleanField(default=False)
@@ -46,19 +47,19 @@ class User(AbstractBaseUser):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
-    objects = UserManager
+    objects = UserManager()
 
     def __str__(self):
         return self.email
 
     def get_short_name(self):
-        if self.first_name:
-            return self.first_name
+        if self.name:
+            return self.name
         return self.email
 
-    def get_last_name(self):
-        if self.last_name:
-            return self.last_name
+    def get_full_name(self):
+        if self.full_name:
+            return self.full_name
         return self.email
 
     def has_perm(self, perm, obj=None):
@@ -78,6 +79,6 @@ class User(AbstractBaseUser):
         return self.admin
 
     def save(self, *args, **kwargs):
-        print(self.password)
+        if not self.id and not self.staff and not self.admin:
+            self.password = make_password(self.password)
         super().save(*args, **kwargs)
-
--- src/post_in/post_in/settings.py
+++ src/post_in/post_in/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
+    'notes',
+    'rest_framework',
 ]
 
 
